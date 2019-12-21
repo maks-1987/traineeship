74bed21d43ff60deab79b7c66e6caa48abd17abe
MaxD <max.moderation@gmail.com>
2019-12-21 19:04:33 +0200

    read inner class

---
+++ module1/src/main/java/net/broscorp/_My_test_package/TestInnerClass.java
@@ -0,0 +1,34 @@
+package net.broscorp._My_test_package;
+
+public class TestInnerClass {
+    class Content {
+        private int i = 11;
+
+        public int value() {
+            return i;
+        }
+    }
+
+    class Destination {
+        private String label;
+
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+
+        String readLabel() {
+            return label;
+        }
+    }
+
+    public void ship(String destinat) {
+        Content cont = new Content();
+        Destination d = new Destination(destinat);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        TestInnerClass testInnerClass = new TestInnerClass();
+        testInnerClass.ship("UA");
+    }
+}
