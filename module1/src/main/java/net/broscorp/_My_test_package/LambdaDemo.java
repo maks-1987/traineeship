80bf89aed61767c87a00634646aabba0487a4fb7
MaxD <max.moderation@gmail.com>
2019-12-24 18:14:48 +0200

    lambda repeat again

--- module1/src/main/java/net/broscorp/_My_test_package/LambdaDemo.java
+++ module1/src/main/java/net/broscorp/_My_test_package/LambdaDemo.java
@@ -1,13 +1,13 @@
 package net.broscorp._My_test_package;
 
 interface A {
-    void show();
+    void show(int i);
 }
 
 class XYZ implements A {
     @Override
-    public void show() {
-        System.out.println("First");
+    public void show(int i) {
+        System.out.println("Simple realisation " + i);
     }
 }
 
@@ -15,17 +15,21 @@ public class LambdaDemo {
     public static void main(String[] args) {
         /** реализация обычная */
         A objXYZ = new XYZ();
-        objXYZ.show();
+        objXYZ.show(4);
+
         /** реализация анонимным классом */
         A obj1 = new A() {
-            public void show() {
-                System.out.println("Two");
+            public void show(int i1) {
+                System.out.println("Anonim class " + i1);
             }
         };
+        obj1.show(4);
+
         /** реализ через лямбду */
-        A obj2 = () -> {
-            System.out.println("Lambda");
+        A obj2 = i2 -> {
+            System.out.println("Lambda " + i2);
         };
+        obj2.show(4);
     }
 }
 
