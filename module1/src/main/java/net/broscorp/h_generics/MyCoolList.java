991bc82d85913ac62bf8b08b430fa3fdc87a0ec2
MaxD <max.moderation@gmail.com>
2020-01-07 17:15:30 +0200

    need to fix

--- module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -1,24 +1,85 @@
 package net.broscorp.h_generics;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public abstract class MyCoolList implements List {
 
-  public void add(Object o){
-    throw new NotImplementedException();
-  }
+    List list = new ArrayList();
+    /*MyCoolList() {
+        List list = new ArrayList();
+    }*/
 
-  public Object get(int index){
-    throw new NotImplementedException();
-  }
 
-  public Object remove(int index){
-    throw new NotImplementedException();
-  }
+    /*public boolean add(Number e) {
+        ensureCapacityInternal(size + 1);  // Increments modCount!!
+        elementData[size++] = e;
+        return true;
+    }
+    public E get(int index) {
+        rangeCheck(index);
 
-  public Object map(Function f){
-    throw new NotImplementedException();
-  }
+        return elementData(index);
+    }
+    public E remove(int index) {
+        rangeCheck(index);
+
+        modCount++;
+        E oldValue = elementData(index);
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index+1, elementData, index,
+                    numMoved);
+        elementData[--size] = null; // clear to let GC do its work
+
+        return oldValue;
+    }
+    public boolean remove(Object o) {
+        if (o == null) {
+            for (int index = 0; index < size; index++)
+                if (elementData[index] == null) {
+                    fastRemove(index);
+                    return true;
+                }
+        } else {
+            for (int index = 0; index < size; index++)
+                if (o.equals(elementData[index])) {
+                    fastRemove(index);
+                    return true;
+                }
+        }
+        return false;
+    }
+    private void fastRemove(int index) {
+        modCount++;
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index+1, elementData, index,
+                    numMoved);
+        elementData[--size] = null; // clear to let GC do its work
+    }*/
+
+
+    public void add(Object o) {
+        list.add(o);
+    }
+
+    public Object get(int index) {
+        get(index);
+        return this;
+    }
+
+    public Object remove(int index) {
+        remove(index);
+        return this;
+    }
+
+    public Object map(Function f) {
+        throw new NotImplementedException();
+    }
 
 }
