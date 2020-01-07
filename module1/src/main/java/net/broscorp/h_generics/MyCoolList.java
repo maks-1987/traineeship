8ba76fd89b5f364593209f27a5243a50eff86a41
MaxD <max.moderation@gmail.com>
2020-01-07 17:26:48 +0200

    need to fix

--- module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -6,80 +6,30 @@ import java.util.function.Function;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public abstract class MyCoolList implements List {
+public class MyCoolList {
 
-    List list = new ArrayList();
-    /*MyCoolList() {
-        List list = new ArrayList();
-    }*/
+    List<Integer> list = new ArrayList<Integer>();
 
-
-    /*public boolean add(Number e) {
-        ensureCapacityInternal(size + 1);  // Increments modCount!!
-        elementData[size++] = e;
-        return true;
-    }
-    public E get(int index) {
-        rangeCheck(index);
-
-        return elementData(index);
-    }
-    public E remove(int index) {
-        rangeCheck(index);
-
-        modCount++;
-        E oldValue = elementData(index);
-
-        int numMoved = size - index - 1;
-        if (numMoved > 0)
-            System.arraycopy(elementData, index+1, elementData, index,
-                    numMoved);
-        elementData[--size] = null; // clear to let GC do its work
-
-        return oldValue;
-    }
-    public boolean remove(Object o) {
-        if (o == null) {
-            for (int index = 0; index < size; index++)
-                if (elementData[index] == null) {
-                    fastRemove(index);
-                    return true;
-                }
-        } else {
-            for (int index = 0; index < size; index++)
-                if (o.equals(elementData[index])) {
-                    fastRemove(index);
-                    return true;
-                }
-        }
-        return false;
-    }
-    private void fastRemove(int index) {
-        modCount++;
-        int numMoved = size - index - 1;
-        if (numMoved > 0)
-            System.arraycopy(elementData, index+1, elementData, index,
-                    numMoved);
-        elementData[--size] = null; // clear to let GC do its work
-    }*/
-
-
-    public void add(Object o) {
+    public void add(Integer o) {
         list.add(o);
     }
 
-    public Object get(int index) {
-        get(index);
-        return this;
+    public Integer get(int index) {
+        //list.get(index);
+        return list.get(index);
     }
 
-    public Object remove(int index) {
-        remove(index);
-        return this;
+    public Integer remove(int index) {
+        //list.remove(index);
+        return list.remove(index);
     }
 
     public Object map(Function f) {
         throw new NotImplementedException();
     }
 
+    @Override
+    public String toString() {
+        return "MyCoolList{" + "list=" + list + '}';
+    }
 }
